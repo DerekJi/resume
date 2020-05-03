b12ea563e53d25f752f334741b44554e425fcbf9
Derek Ji <syfool@gmail.com>
2020-05-03 22:19:59 +0930

    Re-organize scss files & update some details

--- src/app/pages/contact/contact.module.ts
+++ src/app/pages/contact/contact.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from '../contact/contact.component';
+import { LcdNumberComponent } from './lcd-number/lcd-number.component';
 
 
 
 @NgModule({
-  declarations: [ContactComponent],
+  declarations: [ContactComponent, LcdNumberComponent],
   exports: [ContactComponent],
   imports: [
     CommonModule
