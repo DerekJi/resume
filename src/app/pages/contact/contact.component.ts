1d1ec62fb7f7f5e52465bf4016096cd9a0873a79
Derek Ji <syfool@gmail.com>
2020-09-08 22:50:57 +0930

    refactor: update phone number and email

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -7,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  // Anti crawlers
+  get phone(): string {
+    let num = '0';
+    let d = 4;
+    num += (d--).toString();
+    num += d.toString();
+    num += d.toString();
+    num += ' ';
+
+    d = 7;
+    num += (d++).toString();
+    num += (d--).toString();
+    num += (d).toString();
+    num += ' ';
+
+    d = 2;
+    num += (d).toString();
+    num += (d + 6).toString();
+    num += (d).toString();
+    return num;
+  }
+
+
+  get email(): string {
+    let s = 'sy';
+    s += 'fool';
+    s += '@';
+    s += 'hot';
+    s += 'mail';
+    s += '.';
+    s += 'com';
+    return s;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
