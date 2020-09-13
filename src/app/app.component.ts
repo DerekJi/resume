824bdca69f8d777dc4ce798f086c6ed648a55c5d
Derek Ji <syfool@gmail.com>
2020-09-13 15:12:16 +0930

    test(try to use system variables): restored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ScrollService } from '@core/scroll-spy.service';
-declare var process: any;
 
 @Component({
   selector: 'app-root',
@@ -8,8 +7,6 @@ declare var process: any;
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  token = process.env['ACCESS_TOKEN'];
 
   constructor(
     private spyService: ScrollService
