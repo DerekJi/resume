d69874ce4c2441df367cb3f1f1824fc97836c533
Derek Ji <syfool@gmail.com>
2020-09-13 14:28:28 +0930

    test(try to use system variables):

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ScrollService } from '@core/scroll-spy.service';
+import {getInput} from '@actions/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,8 @@ import { ScrollService } from '@core/scroll-spy.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  readonly token: string = getInput('ACCESS_TOKEN');
 
   constructor(
     private spyService: ScrollService
