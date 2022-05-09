3d22a24c8a640e14ffd732d008aaed0004736e8d
Zhigang (Derek) Ji <syfool@gmail.com>
2022-05-09 16:30:20 +0930

    feat: update footer component with the data model

--- src/app/pages/footer/footer.component.ts
+++ src/app/pages/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Resume } from '@core/models/resume';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  @Input() cv: Resume;
 
   constructor() { }
 
